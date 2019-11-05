9006e689c82b73b297b380ca6fbd5bd714a47637
IamTheStitch <46461384+IamTheStitch@users.noreply.github.com>
2019-11-05 15:20:11 -0700

    Created the Base of the Game

---
+++ Assets/Scripts/NarrativeGame.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarrativeGame : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] State startingState;
+    bool purpleCoat;
+    bool silverGauntlet;
+    bool featherHat;
+
+    string[] narratives = {};
+
+    State story;
+
+    void Start()
+    {
+        story = startingState;
+        storyText.text = story.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState(); 
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = story.GetNextStates();
+        if (nextStates.Length < 2 && Input.GetKeyDown(KeyCode.Return))
+        {
+            story = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            story = nextStates[0];
+            if (!purpleCoat)
+            {
+                purpleCoat = true;
+                silverGauntlet = false;
+                featherHat = false;
+            }
+                
+        }
+        else if (nextStates.Length == 2 && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            story = nextStates[1];
+            if (!silverGauntlet)
+            {
+                purpleCoat = false;
+                silverGauntlet = true;
+                featherHat = false;
+            }
+        }
+        else if (nextStates.Length == 3 && Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            story = nextStates[2];
+            if (!featherHat)
+            {
+                purpleCoat = false;
+                silverGauntlet = false;
+                featherHat = true;
+            }
+        }
+        storyText.text = story.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//creates a scriptable asset file called Story
+[CreateAssetMenu(menuName  = "Story")]
+public class State : ScriptableObject
+{
+    //Text Area expands the field of the text area (words,lines)
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
